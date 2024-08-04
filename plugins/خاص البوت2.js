3a59e9f16dd50fdbefa0915a7055da4e2b211ead
boudy <144317574+Boudy1490@users.noreply.github.com>
2024-08-04 13:38:19 +0300

    Update خاص البوت2.js

--- plugins/خاص البوت2.js
+++ plugins/خاص البوت2.js
@@ -3,8 +3,12 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }
   if (isOwner) return false;
   if (m.isGroup) return false;
   if (!m.isGroup) {
-     await m.reply(*[❗] مرحبًا @${m.sender.split@[0]}، يُمنع التحدث في الخاص مع البوت لهذا السبب سيتم حظرك.*\n\n*اذا كنت تريد تجربه البوت ادخل جروب البوت『 https://chat.whatsapp.com/KCr93o7UJW92fOyIQTlqkm 』*\n\n*اذا كنت تريد ان تكلم مطور البوت هذا رقمي 『‏‪『++967730068059*, false, { mentions: [m.sender] });
-      await conn.updateBlockStatus(m.chat, 'block');
-return true;
-    }
+    await m.reply(
+      *[❗] مرحبًا @${m.sender.split('@')[0]}، يُمنع التحدث في الخاص مع البوت لهذا السبب سيتم حظرك.*\n\n*اذا كنت تريد تجربه البوت ادخل جروب البوت『 https://chat.whatsapp.com/KCr93o7UJW92fOyIQTlqkm 』*\n\n*اذا كنت تريد ان تكلم مطور البوت هذا رقمي 『‏‪+967730068059』*,
+      false,
+      { mentions: [m.sender] }
+    );
+    await conn.updateBlockStatus(m.chat, 'block');
+    return true;
   }
+}
