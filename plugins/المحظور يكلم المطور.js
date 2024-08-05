77e488f0ac1c8a72e924e4fec74227faa191d52c
boudy <144317574+Boudy1490@users.noreply.github.com>
2024-08-05 17:06:03 +0300

    Update المحظور يكلم المطور.js

--- plugins/المحظور يكلم المطور.js
+++ plugins/المحظور يكلم المطور.js
@@ -6,7 +6,8 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, كلم المطور لرفع لحظر عنكك المطور: +201151094460 يمنع التحدث بالرسائل الخاصة للبوت وبالتالي سيتم حظرك.*`, false, {mentions: [m.sender]});
+    await m.reply(`*[❗] مرحبا @$, كلم المطور لرفع لحظر عنكك المطور: +201151094460 يمنع التحدث بالرسائل الخاصة للبوت وبالتالي سيتم حظرك 
+او ادخل جروب البوت لاستخدامه https://chat.whatsapp.com/KCr93o7UJW92fOyIQTlqkm.*`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
